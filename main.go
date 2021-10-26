942e8f3ab75e5d7b8f67eb04b40a3d2a6dd1c0d2
Daniil Maslov <i@dmaslov.dev>
2021-10-26 11:46:13 +0300

    Initialize project files

--- main.go
+++ main.go
@@ -1,35 +1,8 @@
 package main
-import (
-	"html/template"
-	"log"
-	"net/http"
-)
 
-func indexHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	//для простоты не обрабатываем ошибки
-	t, _ := template.ParseFiles("test.html")
-	t.Execute(w, nil)
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", indexHandlerFunc)
-	http.HandleFunc("/login", loginHandlerFunc)
-	log.Fatal(http.ListenAndServe("localhost:3050", nil))
-}
-
-func loginHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	//Обрабатываем только POST-запрос
-	if r.Method != "POST" {
-		http.NotFound(w, r)
-	}
-	//для простоты не обрабатываем ошибки
-	r.ParseForm()
-	user := r.FormValue("user")
-	password := r.FormValue("password")
-	//Проверяем логин и пароль
-	if !(user == "zaz600" && password == "123") {
-		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
-		return
-	}
-	w.Write([]byte("hello " + user))
+	a := 3
+	fmt.Println(a)
 }
