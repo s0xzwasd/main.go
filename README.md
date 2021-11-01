5813816747884cbda8647a85278e6168a304c25d
Daniil Maslov <i@dmaslov.dev>
2021-11-01 13:27:48 +0300

    Initialize project files

---
+++ README.md
@@ -0,0 +1,14 @@
+```go
+package main
+
+import (
+  log "github.com/sirupsen/logrus"
+)
+
+func main() {
+  log.WithFields(log.Fields{
+    "animal": "walrus",
+  }).Info("A walrus appears")
+}
+```
+
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
-	a := 3
-	fmt.Println(a)
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+	}).Info("A walrus appears")
 }
